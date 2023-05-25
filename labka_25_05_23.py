115654e3cc5bd17350d32cdfe1f83d1661737d45
student <bukindimon444@gmail.com>
2023-05-25 20:05:29 +0300

    Друге завдання

--- labka_25_05_23.py
+++ labka_25_05_23.py
@@ -1,5 +1,5 @@
 #1
-
+'''
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,6 +10,19 @@ if response.status_code == 200:
     title = soup.find("title").text
     print(title)
 else:
-    print("No connection")
+    print("No connection",response.status_code)
+'''
 #2
 
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://uk.wikipedia.org/")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content , "html.parser")
+    img = soup.find_all("img") #словник
+    for i in img:
+        print("https://" + i["src"])
+
+    with open("file.txt" , "w") as file:
+        file.write(str(i))
