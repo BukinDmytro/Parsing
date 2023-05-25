80c3fd0622ab50422aa75e7b55b1816067582adf
student <bukindimon444@gmail.com>
2023-05-25 18:59:19 +0300

    знайомство з requests

--- classwork_25_05_23.py
+++ classwork_25_05_23.py
@@ -1,5 +1,14 @@
+
 import urllib.request
 opener = urllib.request.build_opener()
 response = opener.open("https://httpbin.org/get")
 print(response.read())
 print("")
+
+
+
+
+import requests
+
+response = requests.get("https://coinmarketcap.com/")
+print(response.text)
