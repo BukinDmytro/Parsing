10546823f72dc803f52162072fa79afd7886ee46
User <bukindimon444@gmail.com>
2023-06-06 15:34:57 +0300

    Домашнє з Вікі-ботом

--- domashka_09_06_23.py
+++ domashka_09_06_23.py
@@ -1,20 +1,29 @@
-import requests
-import wikipedia
-from bs4 import BeautifulSoup
-import random
+
 import telebot
 from telebot import types
-bot = telebot.TeleBot("6003048631:AAHv61_F4QA8cN_oVTw2SRlvWNljQC2TXI4")
+import wikipedia
+import requests
 
+bot = telebot.TeleBot('6003048631:AAHv61_F4QA8cN_oVTw2SRlvWNljQC2TXI4')
+@bot.message_handler(commands=['start'])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Напишіть назву статті:')
+@bot.message_handler(content_types=['text'])
+def handler_text(message):
+    name = message.text
+    wikipedia.set_lang("uk")
+    response = requests.get('https://uk.wikipedia.org/wiki/' + name)
+    if response.status_code == 200:
+        if wikipedia.summary(name, chars=1000):
+            answer = f"Інфа, яку вдалося знайти:\n\n" \
+                     f"{'https://uk.wikipedia.org/wiki/' + name}\n\n" \
+                     f"{wikipedia.summary(name, chars=1000)}\n\nПродовження тут:\n\n" \
+                     f"{'https://uk.wikipedia.org/wiki/' + name}"
 
-result = wikipedia.search(input("Введіть свій запит на англійській мові:"))
+        else:
+            answer = 'Щось пішло не так ('
+    else:
+        answer = f'Статті з назвою "{name}" не знайдено.'
+    bot.send_message(message.chat.id, answer)
 
-for search in result:
-    print(search)
-    print(wikipedia.page(search).summary)
-
-if len(wikipedia.page(search).summary) < 1000:
-    print("Була знайдена стаття на менш ніж 1000 символів або ж не була знайдена взагалі (")
-else:
-    print(wikipedia.page(search).summary)
-
+bot.polling()
